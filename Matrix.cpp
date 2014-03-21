e4c3725006384aec49bf35bb451c84bf3cc8eb4c
adri84 <adrian.salas84@gmail.com>
2014-03-21 16:20:09 +0100

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -35,6 +35,21 @@ void Matrix::Menu()
 	}
 }
 
+void Matrix::Open(string FileName)
+{
+	cout << "Quelle matrice voulez-vous ouvrir? " << endl;
+	
+	do
+	{
+		if(!OpenFile())
+			cout << "Ce fichier n'existe pas" << endl;
+
+		cout << "Entrez le nom de la matrice:";x
+		cin >> Filename;
+
+	} while (!OpenFile());
+}
+
  void Matrix::DisplayDB()
  {
 	//sous entend que les matrices se trouvent dans le dossier DB 
