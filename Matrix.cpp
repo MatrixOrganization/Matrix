59fa904e3378060a427a01a0477e9d697c3506ed
m-dl <fullcontactsoldier@gmail.com>
2014-03-21 16:40:32 +0100

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -35,28 +35,18 @@ void Matrix::Menu()
 	}
 }
 
-void Matrix::Open(string FileName)
-{
-	cout << "Quelle matrice voulez-vous ouvrir? " << endl;
-	
-	do
-	{
-		if(!OpenFile())
-			cout << "Ce fichier n'existe pas" << endl;
-
-		cout << "Entrez le nom de la matrice:";x
-		cin >> Filename;
-
-	} while (!OpenFile());
-}
-
  void Matrix::DisplayDB()
  {
 	//sous entend que les matrices se trouvent dans le dossier DB 
 	system("for i in DB/* do echo i \" \";done");	
  }
  
- bool Sparse() // matrice creuse ?
+ double Matrix::Read_Value(fstream &M, int &l, int &c, int &v) // lit et retourne une valeur de la matrice
+ {
+    M >> l >> c >> v;
+ }
+
+ bool Matrix::Sparse() // matrice creuse ?
 {
     int nb_value_zero = 0, nb_value = 0, percent_sparse = 70; // matrice creuse si 70% des valeurs sont = 0
     string value;
@@ -75,42 +65,3 @@ void Matrix::Open(string FileName)
     return false; // pas creuse
 }
 
-void Matrix::NewMatrix(long height, long weight)
-{
-	int sizeChoice;
-	cout << "Choix de la taille des valeurs" << endl;
-	cout << "Si 0-9: tapez 10; si 0-99: tapez 100; si 0-999: tapez 1000; si 0-9999: tapez 10000" << endl;
-	cin >> sizeChoice;
-
-	string nameFile;
-	cout << "Nom du fichier de la matrice:" << endl << " -> ";
-	cin >> nameFile;
-
-	ofstream newMat(nameFile.c_str(), ios::out | ios::trunc);
-		if(!newMat.is_open())
-    		cout << "Impossible d'ouvrir le fichier en écriture !" << endl;
-
-		else
-		{
-			newMat << "L C V" << endl;
-			for(int i=1; i<height; i++)
-			{
-				for(int j=1; j<weight; j++)
-				{
-					newMat << i << " " << j << " " << rand()%sizeChoice << endl;
-				}
-			}
-			newMat << " " << endl;
-		}
-		newMat.close();
-
-	return 0;
-}
-
-void Matrix::Help(string)
-{
-	if(string=="open")
-		cout << "Permet l'ouverture d'un fichier contenant une matrice (nom du fichier en argument)" << endl;
-	if(string=="displaybdd")
-		cout << "Permet d'afficher les matrices dans la base de données" << endl;
-}
