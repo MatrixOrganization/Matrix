da2c9de0126cee6f7b29b766c443d7f85c3ffee9
ThibautDumalin <thibaut.dumalin@gmail.com>
2014-03-21 16:09:25 +0100

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,41 @@
 #include “Matrix.h”
+
+void Matrix::Begin()
+{
+	cout<< "Bienvenue" << endl;
+	Menu();
+	
+}
+void Matrix::Menu()
+{
+	int command;
+	string s;
+	cout<< "Que voulez vous faire ?" << endl 
+	<< "1-Ouvrir une matrice" << endl 
+	<< "2-Afficher les matrices de la base de donnée"<<endl
+	<< "3-help" <<endl
+	<<"4-Quitter"<<endl;
+	cin>>command;
+	if(command==1)//OPEN
+	{
+		Open();
+	}
+	if(command==2)//Afficher les matrices déjà créé
+	{
+		DisplayDB();
+	}
+	if(command==3)//Help
+	{
+		cin>>s;
+		Help(s);
+	}
+	if(command==4)//quiter
+	{
+		Quit();
+	}
+}
+ void Matrix::DisplayDB()
+ {
+	//sous entend que les matrices se trouvent dans le dossier DB 
+	system("for i in DB/* do echo i \" \";done");	
+ }
