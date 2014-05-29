f44b364113c4fb7045f38a0f0ba4d21a2876a437
adri84 <adrian.salas84@gmail.com>
2014-05-29 14:03:14 +0200

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -10,69 +10,63 @@ class Matrix
 {
   public:
 
-  void Begin();                            // commence l’application
-  void Menu();                            // Affiche les fonctionnalités
+  void Begin();
+  void Menu();
   void ChooseOp();
 
   //Commandes:
 
-  void NewMatrix();                                    // creer une matrice aléatoire
-  void Back();                                          // permet de revenir à l’étape précédente
-  void Delete();                                    // supprime une matrice de la BDD
-  void DisplayDB();                                      // affiche les matrices de la BDD
-                                    // ouvre un fichier txt “s” pour créer la matrice
+  void NewMatrix();
+  void Back();
+  void Delete();
+  void DisplayDB();
 
-  void Open(int );
-  void Help(string);                                     // donne des infos sur chaque opération ou cmd
-  void Quit();                                            // quitte et ferme tous les fichiers ouverts
-                                 // retourne true si le fichier existe/s'est correctement ouvert
-  void CloseFile(int );                               // retourne true si
+  bool CheckOpen(string);
+  bool Open(int);
+  void Help(string);
+  void Quit();
 
-                                   // quitte et ferme tous les fichiers ouverts
+  void CloseFile(int );
 
   //Les fonctions de manipulation sont des bool pour renvoyer false en cas d’erreur
 
   //Manipulation
 
-  bool SoustractionM();                               // soustraction de matrices (M)-(M)
-  bool SoustractionV();                            // soustraction de la matrice par une valeur (M)-Val
+  bool SoustractionM();
+  bool SoustractionV();
 
-  bool AdditionM() ;                              // addition de matrices (M)+(M)
-  bool AdditionV();                                // addition de la matrice par une valeur (M)+Val
+  bool AdditionM();
+  bool AdditionV();
 
-  bool MultiplicationM();                          // multiplication de matrices (M)*(M)
-  bool MultiplicationV();                         // multiplication de la matrice par une valeur (M)*Val
-                              // division de matrices (M)/(M)
-  bool DivisionV();                                // division de la matrice par une valeur (M)/Val
+  bool MultiplicationM();
+  bool MultiplicationV();
 
-  bool Pow();                                    // puissance (M)^v                              // modulo (M)%v
-  bool Compare();                                 // comparaison de matrices (M)&(M)                                         // rang de la matrice (M)                                   // transposée de la matrice (M)
-  bool Identity();                        // créé la matrice identité avec les même dimension que la matrice ouverte
+  bool DivisionV();
+
+  bool Pow();
+  bool Compare();
+  bool Identity();
 
     // Fonctions sur les matrices
 
-  void Read_Value(int ,int&, int&, double&);                                    // lit et retourne une valeur de la matrice
+  void Read_Value(int ,int&, int&, double&);
   void Read_Size(int,int &,int&);
-  bool Sparse();                     // matrice creuse ?
+  bool Sparse();
+
   double Place_to_Coord(int matrix,int line,int col);
   bool Check_Size(char m,int & l,int & c);
 
   void StartTimer();
   void DisplayTimer();
-
+  void FlushCin();
 
 
   private:
 
     clock_t begin, end;
-    double **buffMatrix;                                        // sauvegarde de la matrice dans un fichier.txt
+    double **buffMatrix;
     string FileName;
     fstream M1;
-    fstream M2;                                          // le nom du fichier de la matrice sur laquelle on travaille
+    fstream M2;
     fstream M3;
 };
-
-
-
-
-
